691b1c0eab052eaa1544c6b2dc52dd627092fa34
hudson-newey <hudsonnewey@gmail.com>
2023-11-20 12:57:25 +1000

    Added heat map for completed dates

--- client/src/app/components/calendar-heatmap/calendar-heatmap.component.ts
+++ client/src/app/components/calendar-heatmap/calendar-heatmap.component.ts
@@ -1,11 +1,45 @@
-import { Component } from "@angular/core";
+// @ts-nocheck
+import { AfterViewInit, Component, Input } from "@angular/core";
+import schema from "./calendar-heatmap.schema.json";
+import embed from "vega-embed";
+
+interface CalendarData {
+  date: string;
+  count: number;
+}
 
 @Component({
   selector: "app-calendar-heatmap",
-  templateUrl: "./calendar-heatmap.component.html",
+  template: ` <div id="calendar-element"></div> `,
   styleUrls: ["./calendar-heatmap.component.less"],
 })
-export class CalendarHeatmapComponent {
-  public constructor() {
+export class CalendarHeatmapComponent implements AfterViewInit {
+  public constructor() {}
+
+  @Input() public data: string[] = [];
+  private spec: any = schema;
+  private chart: any;
+
+  public async ngAfterViewInit() {
+    this.spec = this.addDataToSpec(this.data, this.spec);
+
+    this.chart = await embed("#calendar-element", this.spec);
+    
+    console.debug(this.chart);
+  }
+
+  private addDataToSpec(data: string[], spec: any): any {
+    const newSpec = spec;
+    newSpec.data.values = this.formatData(data);
+    return newSpec;
+  }
+
+  private formatData(data: string[]): CalendarData[] {
+    return data.map((date: string): CalendarData => {
+      return {
+        date,
+        count: 1,
+      };
+    });
   }
 }
