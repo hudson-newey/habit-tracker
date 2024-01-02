a75895ed2316f11019795dd441a48f22b0455089
hudson-newey <grath@duck.com>
2024-01-02 23:54:17 +1000

    Changed to use go server

--- client/start.sh
+++ client/start.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
 echo "Warning: This script should only be used for development environments.";
-pnpm start;
+pnpm build;
+
+cd ../clientServer/;
+go build ./clientServer;
+./clientServer ../client/dist/client/index.html
