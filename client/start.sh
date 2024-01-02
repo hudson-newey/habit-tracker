861d53329cb4012b1b8b0110cbf559572fc7fed9
hudson-newey <grath@duck.com>
2024-01-03 00:23:38 +1000

    Made the client server a maual setup

--- client/start.sh
+++ client/start.sh
@@ -3,6 +3,7 @@ rm -rf ./dist/;
 
 pnpm build;
 
-cd ../clientServer/;
-go build ./clientServer;
-./clientServer ../client/dist/client/index.html
+# enable these lines if you want to restart the client server every time
+# cd ../clientServer/;
+# go build ./clientServer;
+#./clientServer ../client/dist/client/index.html
