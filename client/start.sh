6985fa8bd34b3333523123a33724a218856f7a01
hudson-newey <grath@duck.com>
2024-01-03 00:38:25 +1000

    Remove server

--- client/start.sh
+++ client/start.sh
@@ -4,6 +4,6 @@ rm -rf ./dist/;
 pnpm build;
 
 # enable these lines if you want to restart the client server every time
-# cd ../clientServer/;
-# go build ./clientServer;
-#./clientServer ../client/dist/client/index.html
+cd ../clientServer/;
+go build ./clientServer;
+/clientServer ../client/dist/client/index.html
