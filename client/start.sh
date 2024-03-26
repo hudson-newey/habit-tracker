0221a56f7295332bbf756dc72c6f725e65f7fa1c
hudson-newey <grath@duck.com>
2024-03-26 23:59:21 +1000

    Clean up scripts

--- client/start.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-rm -rf ./dist/;
-
-pnpm build;
-
-# enable these lines if you want to restart the client server every time
-# cd ../clientServer/;
-# go build ./clientServer;
-# /clientServer ../client/dist/client/index.htmlhabit
--- server/start.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-echo "Warning: This script should only be used for development environments.";
-go run ./server.go;
