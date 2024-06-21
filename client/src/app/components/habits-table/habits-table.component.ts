5a63c64e6ee3987704e02432f7d39082a62efd29
hudson-newey <grath@duck.com>
2024-06-21 21:57:35 +1000

    Hide anti habits behind dropdown

--- client/src/app/components/habits-table/habits-table.component.ts
+++ client/src/app/components/habits-table/habits-table.component.ts
@@ -1,7 +1,5 @@
 import { Component, EventEmitter, Input, Output } from "@angular/core";
-import { faCheck, faXmark } from "@fortawesome/free-solid-svg-icons";
 import { Habit } from "src/app/models/habit";
-import { FaIconComponent } from "@fortawesome/angular-fontawesome";
 import { RouterLink } from "@angular/router";
 import { NoContextMenuDirective } from "../../directives/no-context-menu.directive";
 import { NgFor, NgIf } from "@angular/common";
@@ -9,23 +7,16 @@ import { NgFor, NgIf } from "@angular/common";
 @Component({
     selector: "app-habits-table",
     templateUrl: "./habits-table.component.html",
-    styleUrls: ["./habits-table.component.less"],
+    styleUrl: "./habits-table.component.less",
     standalone: true,
     imports: [
         NgFor,
         NoContextMenuDirective,
         RouterLink,
         NgIf,
-        FaIconComponent,
     ],
 })
 export class HabitsTableComponent {
-  public constructor() { }
-
-  // font-awesome icons
-  protected faCheck = faCheck;
-  protected faXmark = faXmark;
-
   @Input({ required: true })
   public models: Habit[] = [];
 
@@ -34,4 +25,14 @@ export class HabitsTableComponent {
 
   @Output()
   public decrementState = new EventEmitter<Habit>();
+
+  protected showAntiHabits = false;
+
+  protected get positiveHabits(): Habit[] {
+    return this.models.filter((habit) => !habit.AntiHabit);
+  }
+
+  protected get antiHabits(): Habit[] {
+    return this.models.filter((habit) => habit.AntiHabit);
+  }
 }
